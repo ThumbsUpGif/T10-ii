5c6338ae3a627b846b89cc7ec20d0fda234d3cb1
Magnus J <74900307+ThumbsUpGif@users.noreply.github.com>
2021-10-15 11:35:13 +0200

    Intersectoin Observer is alive and well :-)

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,24 @@
 console.log('Hello World :-)')
+
+
+
+// inView('.when_text')
+// .on('enter', el => div.classList.add('red-background'))
+// .on('exit', el => div.classList.remove('red-background'))
+
+
+const textToRed = document.querySelector('.introduce-fund')
+const graphicOne = document.querySelector('.graphic--1')
+
+console.log(graphicOne)
+console.log(textToRed)
+
+inView('.graphic--1')
+.on('enter', el => {
+textToRed.classList.add('red-background')
+})
+.on('exit', el => {
+textToRed.classList.remove('red-background')
+});
+
+
